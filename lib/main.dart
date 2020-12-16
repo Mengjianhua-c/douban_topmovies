5c7b5e46e7abfbc296073e50fc24eb9541d6c3dc
mengjianhua <mengjianhua@bilibili.com>
2020-12-16 18:47:05 +0800

    topic add

--- lib/main.dart
+++ lib/main.dart
@@ -2,13 +2,14 @@ import 'package:douban_topmovies/services/cli.dart';
 import 'package:flutter/material.dart';
 import 'env.dart';
 import 'routes/routes.dart';
+// ignore: unused_import
 import 'services/real_api.dart';
 import 'services/mock_api.dart';
 import 'blocs/bloc_provider.dart';
 import 'blocs/movies/movies_bloc.dart';
+// ignore: unused_import
 import 'package:douban_topmovies/pages/home.dart';
 import 'package:bot_toast/bot_toast.dart';
-
 
 void main() {
   dio.interceptors.add(CustomInterceptors());
--- lib/pages/tabs/topics.dart
+++ lib/pages/tabs/topics.dart
@@ -1,13 +1,33 @@
 import 'package:flutter/material.dart';
 
+import '../../models/topic/topics_list_entity.dart';
+
 class TopicsListPage extends StatefulWidget {
   @override
   _TopicsListPageState createState() => _TopicsListPageState();
 }
 
 class _TopicsListPageState extends State<TopicsListPage> {
+  var topicsList = new TopicsListEntity().data.topics;
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Container(
+        child: ListView.separated(
+            itemBuilder: (context, index) {
+              var topic = topicsList[index];
+              return ListTile(
+                title: Text(topic.title),
+                subtitle: Text(topic.name),
+                trailing: Text(topic.ctime),
+                onTap: () {
+                  Navigator.pushNamed(context, '/topic/detail',
+                      arguments: topic);
+                },
+              );
+            },
+            separatorBuilder: (context, index) => Divider(
+                  height: .0,
+                ),
+            itemCount: this.topicsList.length));
   }
 }
